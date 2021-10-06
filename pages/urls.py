9d462bf4230ced188f7b5e57dc206d65141a6f1e
= <epautsch@luc.edu>
2021-10-06 14:14:00 -0500

    pages app updates w/ new pages

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import (HomePageView, 
+	AboutPageView, 
+	TeamPageView, 
+	MacroPageView,
+	PortfolioPageView,
+	DepartmentsPageView,
+	GetInvolvedPageView)
 
 urlpatterns = [
+	path('getinvolved/', GetInvolvedPageView.as_view(), name='getinvolved'),
+	path('departments/', DepartmentsPageView.as_view(), name='departments'),
+	path('portfolio/', PortfolioPageView.as_view(), name='portfolio'),
+	path('macro/', MacroPageView.as_view(), name='macro'),
+	path('team/', TeamPageView.as_view(), name='team'),
 	path('about/', AboutPageView.as_view(), name='about'),
 	path('', HomePageView.as_view(), name='home'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,18 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
 	template_name = 'about.html'
+
+class TeamPageView(TemplateView):
+	template_name = 'team.html'
+
+class MacroPageView(TemplateView):
+	template_name = 'macro.html'
+
+class PortfolioPageView(TemplateView):
+	template_name = 'portfolio.html'
+
+class DepartmentsPageView(TemplateView):
+	template_name = 'departments.html'
+
+class GetInvolvedPageView(TemplateView):
+	template_name = 'getinvolved.html'
